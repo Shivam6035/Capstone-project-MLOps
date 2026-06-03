e253399084ca45bf1b3c2679db890d8cfdaaaabe
Shivam6035 <shivamzack6035@gmail.com>
2026-06-04 02:46:59 +0530

    cicd3

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -86,8 +86,7 @@ def main():
         # test_size = 0.2
         
         df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
-        #
-        # df = s3.fetch_file_from_s3("data.csv")S
+     
 
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=42)
